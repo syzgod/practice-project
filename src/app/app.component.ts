e065ff3ec4bfe2566ce3b2269cd5c762aae5977f
syzgod <viktor.berczeli@gmail.com>
2024-03-15 12:00:01 +0100

    Add login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
 import { InputTextModule } from 'primeng/inputtext';
 import { PrimeNGConfig } from 'primeng/api';
 import { RouterOutlet } from '@angular/router';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, InputTextModule],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    InputTextModule,
+    CheckboxModule,
+    StyleClassModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
